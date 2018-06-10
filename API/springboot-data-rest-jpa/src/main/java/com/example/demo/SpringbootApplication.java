f855e6491d145ae0e481ad845ca6c1a3e454bb94
ratnesh <ratneshschauhan@gmail.com>
2018-06-10 20:55:15 +0530

    Initial Commit

---
+++ API/springboot-data-rest-jpa/src/main/java/com/example/demo/SpringbootApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+
+@SpringBootApplication
+@Configuration
+@ComponentScan(basePackages = "com.example")
+@EnableAutoConfiguration
+@EntityScan("com.example")
+@EnableJpaRepositories("com.example")
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+}
